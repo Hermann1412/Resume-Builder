b6732f2bbfd669d62b0ab14e4a6360c18ca78ba0
Hermann <u6411076@au.edu>
2026-03-10 16:33:24 +0700

    template selector

---
+++ client/src/components/TemplateSelector.jsx
@@ -0,0 +1,71 @@
+import { Layout } from "lucide-react";
+import React, { useState } from "react";
+
+const TemplateSelector = ({ selectedTemplate, onChange }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const templates = [
+    {
+      id: "classic",
+      name: "Classic",
+      preview:
+        "A clean, traditional resume format with clear sections and professional typography",
+    },
+    {
+      id: "modern",
+      name: "Modern",
+      preview:
+        "A sleek, contemporary resume format with bold design elements and creative layouts",
+    },
+    {
+      id: "minimal",
+      name: "Minimal",
+      preview:
+        "A simple, elegant resume format with ample white space and a focus on content",
+    },
+    {
+      id: "minimal-image",
+      name: "Minimal with Image",
+      preview:
+        "A minimalist resume format that includes a profile picture for a personal touch",
+    },
+  ];
+  return (
+    <div className="relative">
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="flex items-center gap-1 text-sm text-gray-600 bg-gradient-to-br from-blue-50 to-blue-100 ring-blue-300 hover:text-blue-900 transition-all px-3 oy-2 rounded-lg"
+      >
+        <Layout size={14} /> <span className="max-sm:hidden">Template</span>
+      </button>
+      {isOpen && (
+        <div className="absolute top-full w-xs p-3 mt-2 space-y-3 z-10 bg-white rounded-mf border border-blue-200 shadow">
+          {templates.map((template) => (
+            <div
+              key={template.id}
+              onClick={() => {
+                onChange(template.id);
+                setIsOpen(false);
+              }}
+              className={`relative p-3 border rounded-md cursor-pointer hover:bg-blue-100 ${selectedTemplate === template.id ? "border-blue-400 bg-blue-100" : "border-gray-200 hover:border-gray-400 hover:bg-gray-100"}`}
+            >
+              {selectedTemplate === template.id && (
+                <div className="absolute top-2 right-2">
+                  <div className="size-5 bg-blue-400 rounded-full flex items-center justify-center">
+                    <check className="w-3 h-3 text-white" />
+                  </div>
+                </div>
+              )}
+              <div className="space-y-1">
+                <h4 className="font-medium text-gray-800">{template.name}</h4>
+                <div className="mt-2 p-2 bg-blue-50 rounded text-xs text-gray-500 italic">{template.preview}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TemplateSelector;
--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -4,6 +4,7 @@ import { dummyResumeData } from "../assets/assets";
 import { ArrowLeftIcon, Briefcase, FileText, FolderIcon, GraduationCap, Icon, Sparkle, User, ChevronLeft, ChevronRight } from "lucide-react";
 import PersonnalInfoForm from "../components/PersonalInfoForm";
 import ResumePreview from "../components/resumePreview";
+import TemplateSelector from "../components/TemplateSelector";
 
 
 const ResumeBuilder = () => {
@@ -71,7 +72,10 @@ const ResumeBuilder = () => {
 
               {/* Section Navigation*/}
               <div className="flex justify-between items-center mb-6 border-b border-gray-200 py-1">
-                <div></div>
+                <div className="flex-justify-between items-center mb-6 border-b border-gray-300 py-1">
+                  <TemplateSelector selectedTemplate={resumeData.template} onChange={(template)=> setResumeData(prev => ({...prev, template}))}/>
+                </div>
+
                 <div className="flex items-center">
                   {activeSectionIndex !== 0 && (
                     <button onClick={()=> setActiveSectionIndex((prevIndex)=> Math.max(prevIndex - 1, 0))} className="flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all" disabled={activeSectionIndex === 0}>
